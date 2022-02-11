01ac5d7e35dc1bfd3ec48405f72bdf5daa2c11e1
breanner06 <92890666+breanner06@users.noreply.github.com>
2022-02-11 09:47:02 -0600

    Update assignment5.js

--- assignment5.js
+++ assignment5.js
@@ -1,1 +1,6 @@
 /*Loops and DOM
+This criterion is linked to a Learning OutcomeFor Loop
+This criterion is linked to a Learning OutcomeWhile Loop
+Change CSS with JS
+Put it together (beyond the video)
+Change CSS based upon a variable value
